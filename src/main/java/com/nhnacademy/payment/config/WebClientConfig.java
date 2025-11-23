cfe93ff00bd13dd85aecdd505054b51eaef57e1d
최지훈 <wlgns525359@gmail.com>
2025-11-23 15:54:56 +0900

    refactor : 결제 리팩토링

--- src/main/java/com/nhnacademy/payment/config/WebClientConfig.java
+++ src/main/java/com/nhnacademy/payment/config/WebClientConfig.java
@@ -1,14 +1,37 @@
 package com.nhnacademy.payment.config;
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 
 @Configuration
 public class WebClientConfig {
         @Bean
-        public WebClient webClient() {
-            return WebClient.builder().build();
+        public WebClient tossWebClient() {
+            HttpClient httpClient = HttpClient.create()
+                    .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000) //5초 안에 연결 실패시 에러
+                    .responseTimeout(Duration.ofSeconds(10))//연결을 됐는데 10초안에 응답 없을시 에러
+                    .doOnConnected(conn ->
+                            conn.addHandlerLast(new ReadTimeoutHandler(10, TimeUnit.SECONDS)) //외부 서버에서 응답을 주다가 응답이 끊긴 경우 10초 대기후 연결 끊어버림
+                                    .addHandlerLast(new WriteTimeoutHandler(10, TimeUnit.SECONDS)));//우리 쪽 서버에서 문제가 생긴 경우 10초 동안 전송이 안되면 끊어버림
+
+            return WebClient.builder()
+                    .baseUrl("https://api.tosspayments.com/v1")
+                    .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                    .clientConnector(new ReactorClientHttpConnector(httpClient))
+                    .build();
+
         }
 
 }
--- src/main/java/com/nhnacademy/payment/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/nhnacademy/payment/service/impl/PaymentServiceImpl.java
@@ -4,168 +4,73 @@ package com.nhnacademy.payment.service.impl;
 import com.nhnacademy.order.order.domain.Order;
 import com.nhnacademy.order.order.exception.OrderNotFoundException;
 import com.nhnacademy.order.order.repository.OrderRepository;
+import com.nhnacademy.payment.config.TossPaymentClient;
 import com.nhnacademy.payment.domain.Payment;
 import com.nhnacademy.payment.domain.PaymentStatus;
 import com.nhnacademy.payment.dto.reqeust.PaymentRequestDto;
 import com.nhnacademy.payment.dto.response.PaymentResponse;
 import com.nhnacademy.payment.dto.response.TossPaymentResponseDto;
+import com.nhnacademy.payment.exception.PaymentNotFoundException;
 import com.nhnacademy.payment.repository.PaymentRepository;
 import com.nhnacademy.payment.service.PaymentService;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 
-import java.nio.charset.StandardCharsets;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Base64;
-import java.util.Map;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class PaymentServiceImpl implements PaymentService {
     private final PaymentRepository paymentRepository;
-    private final OrderRepository orderRepository;
-    private final WebClient webClient;
 
-
-    @Value("${toss.secret-key}")
-    private String tossSecretKey;
-
-    //결제 승인
+    //DB에 담는 시점에서만 Transactional 호출하면 됨.
     @Override
     @Transactional
-    public PaymentResponse ConfirmPayment(PaymentRequestDto request) {
+    public PaymentResponse savePayment(TossPaymentResponseDto response, Order order) {
 
-        Order order = orderRepository.findByOrderNumber(request.orderNumber())
-                .orElseThrow(() -> new OrderNotFoundException(request.orderNumber()));
-
-
-        String encodedSecretKey = Base64.getEncoder()
-                .encodeToString((tossSecretKey + ":").getBytes(StandardCharsets.UTF_8));
-
-        TossPaymentResponseDto response;
-
-        try {
-             response = webClient.post()
-                    .uri("https://api.tosspayments.com/v1/payments/confirm")
-                    .header(HttpHeaders.AUTHORIZATION, "Basic " + encodedSecretKey)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .bodyValue(Map.of(
-                            "paymentKey", request.paymentKey(),
-                            "orderId", request.orderNumber(),
-                            "amount", request.amount()
-                    ))
-                    .retrieve()
-                    .bodyToMono(TossPaymentResponseDto.class)
-                    .block();
-        }catch(Exception e) {
-            log.error("TossApi 연동 실페 : {} ", e.getMessage());
-            throw new RuntimeException("결제 승인 중 오류 발생");
-        }
-            if (response != null && "DONE".equals(response.getStatus())) {
-
-                LocalDateTime approvedAt = LocalDateTime.parse(response.getApprovedAt(), DateTimeFormatter.ISO_OFFSET_DATE_TIME);
-                LocalDateTime requestedAt = LocalDateTime.parse(response.getRequestedAt(), DateTimeFormatter.ISO_OFFSET_DATE_TIME);
-
-                Payment payment = Payment.builder()
-                        .paymentKey(response.getPaymentKey())
-                        .paymentStatus(PaymentStatus.DONE)
-                        .paymentRequestAt(requestedAt) // 혹은 response.requestedAt
-                        .paymentApprovedAt(approvedAt)
-                        .paymentReceipt(response.getReceipt().getUrl())
-                        .order(order)
-                        .build();
-                try{
-                    paymentRepository.save(payment);
-                    //todo 주문의 결제 상태값 변환하기
-                    payment.getOrder().setPaymentStatus(com.nhnacademy.order.order.domain.PaymentStatus.COMPLETED);
-                    return PaymentResponse.builder()
-                            .paymentId(payment.getPaymentId())
-                            .orderNumber(payment.getOrder().getOrderNumber())
-                            .status(payment.getPaymentStatus().toString())
-                            .requestedAt(payment.getPaymentRequestAt())
-                            .approvedAt(payment.getPaymentApprovedAt())
-                            .receiptUrl(payment.getPaymentReceipt())
-                            .build();
-
-                }catch(Exception e) {
-                    log.error("결제 중 오류 발생, orderId :{}, paymentKey : {}",request.orderNumber(),request.paymentKey());
-                    //todo 결제 취소 호출
-                    cancelPayment(request.orderNumber(), request.paymentKey());
-                    throw new RuntimeException("결제는 승인도중 오류가 발새앟여 롤백");
-                }
-            }else{
-                throw new RuntimeException("결제 실패 알수 없는 오류");
-            }
-
-    }
-
-    //결제 조회
-    @Override
-    @org.springframework.transaction.annotation.Transactional(readOnly = true)
-    public PaymentResponse getPaymentById(Long paymentId) {
-
-        Payment payment = paymentRepository.findById(paymentId)
-                .orElseThrow(() -> new RuntimeException("결제 정보 없음"));
-
-
-        return PaymentResponse.builder()
-                .paymentId(payment.getPaymentId())
-                .orderNumber(payment.getOrder().getOrderNumber())
-                .status(payment.getPaymentStatus().toString())
-                .totalAmount(payment.getOrder().getOrderDetails().totalPrice())
-                .status(payment.getPaymentStatus().toString())
-                .requestedAt(payment.getPaymentRequestAt())
-                .approvedAt(payment.getPaymentApprovedAt())
-                .receiptUrl(payment.getPaymentReceipt())
+        Payment savePayment = Payment.builder()
+                .paymentKey(response.getPaymentKey())
+                .paymentStatus(PaymentStatus.DONE)
+                .paymentRequestAt(LocalDateTime.parse(response.getRequestedAt(), DateTimeFormatter.ISO_OFFSET_DATE_TIME))
+                .paymentApprovedAt(LocalDateTime.parse(response.getApprovedAt(), DateTimeFormatter.ISO_OFFSET_DATE_TIME))
+                .paymentReceipt(response.getReceipt().getUrl())
+                .order(order)
                 .build();
+
+        paymentRepository.save(savePayment);
+
+        order.setPaymentStatus(com.nhnacademy.order.order.domain.PaymentStatus.COMPLETED);
+        return PaymentResponse.from(savePayment);
     }
 
-
-    //결제 취소
     @Override
     @Transactional
-    public void cancelPayment(String orderNumber,String cancelReason    ) {
-        Payment payment = paymentRepository.findByOrder_OrderNumber(orderNumber);
+    public void updatePaymentCanceledStatus(Payment payment) {
+        payment.cancelPayment();
+        payment.getOrder().setPaymentStatus(com.nhnacademy.order.order.domain.PaymentStatus.CANCELED);
+    }
 
-        Order findOrder = orderRepository.findByOrderNumber(orderNumber).orElseThrow(
-                () -> new OrderNotFoundException(orderNumber)
+    @Override
+    @Transactional(readOnly = true)
+    public Payment getPaymentByOrderNumber(String orderNumber) {
+        Payment payment =  paymentRepository.findByOrder_OrderNumber(orderNumber);
+        if(payment==null){
+            throw new PaymentNotFoundException("결제 정보가 존재하지 않습니다.");
+        }
+        return payment;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public PaymentResponse getPaymentById(Long paymentId) {
+        Payment payment = paymentRepository.findById(paymentId).orElseThrow(
+                () -> new PaymentNotFoundException("결제 정보가 존재하지 않습니다.")
         );
 
-        //해당 주문에 대한 결제 키를 가져옴
-        String paymentKey = payment.getPaymentKey();
-
-        String encodedSecretKey = Base64.getEncoder()
-                .encodeToString((tossSecretKey + ":").getBytes(StandardCharsets.UTF_8));
-
-        try{
-            TossPaymentResponseDto response = webClient.post()
-                    .uri("https://api.tosspayments.com/v1/payments/" + paymentKey + "/cancel")
-                    .header(HttpHeaders.AUTHORIZATION, "Basic "+ encodedSecretKey)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .bodyValue(Map.of("cancelReason",cancelReason))
-                    .retrieve()
-                    .bodyToMono(TossPaymentResponseDto.class)
-                    .block();
-
-            if(response != null && ("CANCELED".equals(response.getStatus()))) {
-                payment.cancelPayment();
-                findOrder.setPaymentStatus(com.nhnacademy.order.order.domain.PaymentStatus.CANCELED);
-
-                log.info("결제 취소 완료 : paymentId={}, reason={}", orderNumber, cancelReason);
-            }else{
-                throw new RuntimeException("토스 api 취소 실패");
-            }
-        }catch(Exception e){
-            log.error("결제 취소 중 오류 : {}",e.getMessage());
-            throw new RuntimeException(e.getMessage());
-        }
+        return PaymentResponse.from(payment);
     }
 }
