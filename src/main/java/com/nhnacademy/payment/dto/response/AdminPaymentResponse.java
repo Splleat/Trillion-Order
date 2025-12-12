f6dcd99496dfb07e6b022cbcb699796ada195c9b
최지훈 <wlgns525359@gmail.com>
2025-12-12 18:33:19 +0900

    fix : 파일 복구

--- src/main/java/com/nhnacademy/payment/dto/response/AdminPaymentResponse.java
+++ src/main/java/com/nhnacademy/payment/dto/response/AdminPaymentResponse.java
@@ -1,16 +1,5 @@
-/*
- * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
- * + Copyright 2025. NHN Academy Corp. All rights reserved.
- * + * While every precaution has been taken in the preparation of this resource,  assumes no
- * + responsibility for errors or omissions, or for damages resulting from the use of the information
- * + contained herein
- * + No part of this resource may be reproduced, stored in a retrieval system, or transmitted, in any
- * + form or by any means, electronic, mechanical, photocopying, recording, or otherwise, without the
- * + prior written permission.
- * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
- */
-
 package com.nhnacademy.payment.dto.response;
+
 
 import com.nhnacademy.payment.entity.Payment;
 import lombok.Builder;
@@ -18,7 +7,7 @@ import lombok.Builder;
 import java.time.LocalDateTime;
 
 @Builder
-public record AdminPaymentResponse (
+public record AdminPaymentResponse(
         Long paymentId,
         String paymentKey,
         String orderNumber, //주문 번호
