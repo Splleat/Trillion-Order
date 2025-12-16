3a3233fe9678072ba971f06f5bd6023368db3d62
Splleat <Splleat@gmail.com>
2025-12-16 10:45:06 +0900

    feat: 대부분의 프레임워크 로그를 숨기고(기본 로그 레벨을 WARN으로 설정), 비즈니스 로그 레벨만 INFO로 출력

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,13 +6,13 @@ INSERT INTO "packaging" ("packaging_type", "packaging_price") VALUES ('WOOD', 50
 INSERT INTO "packaging" ("packaging_type", "packaging_price") VALUES ('STONE', 1000);
 INSERT INTO "packaging" ("packaging_type", "packaging_price") VALUES ('IRON', 2000);
 
--- 샘플 주문 1: 회원 주문 - 완료된 상태 (ID: 1)
+-- 샘플 주문 1: 회원 주문 - 완료된 상태 (ID: 101)
 INSERT INTO "order" ("order_id", "order_number", "member_id", "non_member_password", "order_status",
                      "orderer_name", "orderer_contact",
                      "receiver_name", "receiver_contact", "receiver_address",
                      "order_date", "shipping_post_code", "delivery_date", "delivery_fee", "point_usage", "coupon_discount_amount",
                      "origin_price", "total_price", "coupon_id")
-VALUES (1, 'ORD-MEMBER-COMPLETED-001', 1, null, 'COMPLETED',
+VALUES (101, 'ORD-MEMBER-COMPLETED-001', 1, null, 'COMPLETED',
         '회원주문완료자', '010-1111-2222',
         '회원수령완료인', '010-3333-4444', '서울특별시 강남구 테헤란로 123',
         CURRENT_TIMESTAMP - INTERVAL '10' DAY, '06134', CURRENT_TIMESTAMP - INTERVAL '7' DAY, 0, 1000, 0,
@@ -20,18 +20,18 @@ VALUES (1, 'ORD-MEMBER-COMPLETED-001', 1, null, 'COMPLETED',
 
 -- 샘플 주문 1 상품
 INSERT INTO "order_item" ("orderitem_id", "order_id", "book_id", "book_name", "book_image", "quantity", "price", "coupon_discount_amount", "shipping_date", "packaging_price", "orderitem_status")
-VALUES (1, 1, 1, '자바의 정석', '/images/book1.jpg', 2, 25000, 0, CURRENT_TIMESTAMP - INTERVAL '8' DAY, 500, 'CONFIRMED');
+VALUES (101, 101, 1, '자바의 정석', '/images/book1.jpg', 2, 25000, 0, CURRENT_TIMESTAMP - INTERVAL '8' DAY, 500, 'CONFIRMED');
 
 INSERT INTO "order_item" ("orderitem_id", "order_id", "book_id", "book_name", "book_image", "quantity", "price", "coupon_discount_amount", "shipping_date", "packaging_price", "orderitem_status")
-VALUES (2, 1, 2, '클린 코드', '/images/book2.jpg', 1, 10000, 0, CURRENT_TIMESTAMP - INTERVAL '8' DAY, 0, 'CONFIRMED');
+VALUES (102, 101, 2, '클린 코드', '/images/book2.jpg', 1, 10000, 0, CURRENT_TIMESTAMP - INTERVAL '8' DAY, 0, 'CONFIRMED');
 
--- 샘플 주문 2: 회원 주문 - 취소된 상태 (ID: 2)
+-- 샘플 주문 2: 회원 주문 - 취소된 상태 (ID: 102)
 INSERT INTO "order" ("order_id", "order_number", "member_id", "non_member_password", "order_status",
                      "orderer_name", "orderer_contact",
                      "receiver_name", "receiver_contact", "receiver_address",
                      "order_date", "shipping_post_code", "delivery_date", "delivery_fee", "point_usage", "coupon_discount_amount",
                      "origin_price", "total_price", "coupon_id")
-VALUES (2, 'ORD-MEMBER-CANCELED-002', 1, null, 'CANCELED',
+VALUES (102, 'ORD-MEMBER-CANCELED-002', 1, null, 'CANCELED',
         '회원주문취소자', '010-5555-6666',
         '회원수령취소인', '010-7777-8888', '부산광역시 해운대구 센텀중앙로 10',
         CURRENT_TIMESTAMP - INTERVAL '5' DAY, '48058', CURRENT_TIMESTAMP + INTERVAL '2' DAY, 3000, 0, 0,
@@ -39,18 +39,18 @@ VALUES (2, 'ORD-MEMBER-CANCELED-002', 1, null, 'CANCELED',
 
 -- 샘플 주문 2 상품
 INSERT INTO "order_item" ("orderitem_id", "order_id", "book_id", "book_name", "book_image", "quantity", "price", "coupon_discount_amount", "shipping_date", "packaging_price", "orderitem_status")
-VALUES (3, 2, 3, 'HTTP 완벽 가이드', '/images/book3.jpg', 1, 15000, 0, null, 1000, 'CANCELED');
+VALUES (103, 102, 3, 'HTTP 완벽 가이드', '/images/book3.jpg', 1, 15000, 0, null, 1000, 'CANCELED');
 
 INSERT INTO "order_item" ("orderitem_id", "order_id", "book_id", "book_name", "book_image", "quantity", "price", "coupon_discount_amount", "shipping_date", "packaging_price", "orderitem_status")
-VALUES (4, 2, 4, '객체지향의 사실과 오해', '/images/book4.jpg', 1, 15000, 0, null, 0, 'CANCELED');
+VALUES (104, 102, 4, '객체지향의 사실과 오해', '/images/book4.jpg', 1, 15000, 0, null, 0, 'CANCELED');
 
--- 샘플 주문 3: 비회원 주문 - 부분 반품 상태 (ID: 3)
+-- 샘플 주문 3: 비회원 주문 - 부분 반품 상태 (ID: 103)
 INSERT INTO "order" ("order_id", "order_number", "member_id", "non_member_password", "order_status",
                      "orderer_name", "orderer_contact",
                      "receiver_name", "receiver_contact", "receiver_address",
                      "order_date", "shipping_post_code", "delivery_date", "delivery_fee", "point_usage", "coupon_discount_amount",
                      "origin_price", "total_price", "coupon_id")
-VALUES (3, 'ORD-GUEST-PARTIAL-003', null, '$2a$12$CHTyAGNLcQI9JdQHplZoCucsC1IgBdfnJPGg4R9WZ01sYOPrWYmR2', 'COMPLETED', -- 비밀번호: 1234
+VALUES (103, 'ORD-GUEST-PARTIAL-003', null, '$2a$12$CHTyAGNLcQI9JdQHplZoCucsC1IgBdfnJPGg4R9WZ01sYOPrWYmR2', 'COMPLETED', -- 비밀번호: 1234
         '비회원주문자', '010-9999-0000',
         '비회원수령인', '010-1010-2020', '대전광역시 유성구 대학로 99',
         CURRENT_TIMESTAMP - INTERVAL '20' DAY, '34134', CURRENT_TIMESTAMP - INTERVAL '15' DAY, 0, 0, 0,
@@ -58,7 +58,7 @@ VALUES (3, 'ORD-GUEST-PARTIAL-003', null, '$2a$12$CHTyAGNLcQI9JdQHplZoCucsC1IgBd
 
 -- 샘플 주문 3 상품
 INSERT INTO "order_item" ("orderitem_id", "order_id", "book_id", "book_name", "book_image", "quantity", "price", "coupon_discount_amount", "shipping_date", "packaging_price", "orderitem_status")
-VALUES (5, 3, 1, '자바의 정석', '/images/book1.jpg', 1, 20000, 0, CURRENT_TIMESTAMP - INTERVAL '18' DAY, 0, 'CONFIRMED');
+VALUES (105, 103, 1, '자바의 정석', '/images/book1.jpg', 1, 20000, 0, CURRENT_TIMESTAMP - INTERVAL '18' DAY, 0, 'CONFIRMED');
 
 INSERT INTO "order_item" ("orderitem_id", "order_id", "book_id", "book_name", "book_image", "quantity", "price", "coupon_discount_amount", "shipping_date", "packaging_price", "orderitem_status")
-VALUES (6, 3, 2, '클린 코드', '/images/book2.jpg', 1, 30000, 0, CURRENT_TIMESTAMP - INTERVAL '18' DAY, 1000, 'RETURNED');
+VALUES (106, 103, 2, '클린 코드', '/images/book2.jpg', 1, 30000, 0, CURRENT_TIMESTAMP - INTERVAL '18' DAY, 1000, 'RETURNED');
