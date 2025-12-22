642c2b006d376e11e9b1886f87f5cceae083f7b4
YI SEUNGHYEON <tmdgus0698@gmail.com>
2025-12-22 17:52:10 +0900

    feat: 유효성 검사 추가

--- src/main/java/com/nhnacademy/cart/controller/CartCreateRequestDto.java
+++ src/main/java/com/nhnacademy/cart/controller/CartCreateRequestDto.java
@@ -15,6 +15,6 @@ class CartCreateRequestDto {
 
     @NotNull(message = "수량 값은 필수입니다.")
     @Min(value = 1, message = "수량은 최소 1개 이상이어야 합니다.")
-    @Max(value = 1_000_000_000, message = "수량은 최대 1억개까지 가능합니다.")
+    @Max(value = 100_000_000, message = "수량은 최대 1억개까지 가능합니다.")
     private Integer cartQuantity;
 }
